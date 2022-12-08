ef86b473adda0ebb7ccdd1f6703f4273e31371c6
robrajcs <robrajcs@gmail.com>
2022-12-08 05:09:41 +0100

    3

--- lib.js
+++ lib.js
@@ -3,14 +3,12 @@ const mail = require('./mail');
 
 // Testing numbers 
 module.exports.absolute = function(number) {
-  if (number > 0) return number; 
-  if (number < 0) return -number; 
-  return 0; 
+  return (number >= 0) ? number : -number;
 }
 
 // Testing strings 
 module.exports.greet = function(name) { 
-  return 'Welcome ' + name; 
+  return 'Welcome ' + name + '!'; 
 }
 
 // Testing arrays 
@@ -20,13 +18,12 @@ module.exports.getCurrencies = function() {
 
 // Testing objects 
 module.exports.getProduct = function(productId) { 
-  return { id: productId, price: 10 };
+  return { id: productId, price: 10, category: 'a' };
 }
 
 // Testing exceptions 
 module.exports.registerUser = function(username) { 
   if (!username) throw new Error('Username is required.');
-
   return { id: new Date().getTime(), username: username }
 }
 
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,4 +1,6 @@
 const lib = require('../lib');
+const db = require('../db');
+const mail = require('../mail');
 
 describe('absolute', () => {
     it('should return a positive if input is positive', 
@@ -20,3 +22,70 @@ describe('absolute', () => {
     });
 
 })
+
+
+describe('greet', () => {
+    it('should return the greeting message', () => {
+        const result = lib.greet('Mosh');
+        expect(result).toMatch(/Mosh/);
+    })
+})
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies();
+        // expect(result).toContain('USD');
+        // expect(result).toContain('AUD');
+        // expect(result).toContain('EUR');
+        expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']));
+    })
+})
+
+describe('getProduct', () => { 
+    it('should return the product with the given id', () => {
+        const result = lib.getProduct(1);
+        // expect(result).toEqual({ id: 1, price: 10 });
+        // expect(result).toMatchObject({ id: 1, price: 10 });
+        expect(result).toHaveProperty('id', 1);
+    })    
+})
+
+describe('registerUser', () => { 
+    it('should throw if username is falsy', () => {
+        const args = [null, undefined, NaN, '', 0, false];
+        args.forEach(a => {
+            expect(() => {lib.registerUser(null)}).toThrow();
+        })    
+    });
+
+    it('should return a user object if valid username is passed', () => {
+        const result = lib.registerUser('Mosh');
+        expect(result).toMatchObject({username: 'Mosh'});
+        expect(result.id).toBeGreaterThan(0);
+    })
+});
+
+describe('applyDiscount', () => {
+    it('should apply 10% discount if customer has more than 10 points', () => {
+        db.getCustomerSync = function(customerId) {
+            console.log('Fake reading customer...')
+            return {id: customerId, points: 20}
+        };
+
+        const order = {customerId: 1, totalPrice:10};
+        lib.applyDiscount(order);
+        expect(order.totalPrice).toBe(9);
+    });
+});
+
+describe('notifyCustomer', () => {
+    it('should send an email to the customer', () => {
+        db.getCustomerSync = jest.fn().mockReturnValue({email: 'a'});
+        mail.send = jest.fn();
+
+        lib.notifyCustomer({customerId: 1});
+        expect(mail.send).toHaveBeenCalled();
+        expect(mail.send.mock.calls[0][0]).toBe('a');
+        expect(mail.send.mock.calls[0][1]).toMatch(/order/);
+    });
+});
